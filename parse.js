8b213133b06262a5432b06759d21f7221348683e
YounGoat <youngoat@163.com>
2017-12-30 21:05:32 +0800

    Auto committed by YUPP

--- parse.js
+++ parse.js
@@ -3,15 +3,18 @@
 const MODULE_REQUIRE = 1
     /* built-in */
     , util = require('util')
-    
+
     /* NPM */
-    
+
     /* in-package */
     , absorb = require('jinang/absorb')
+    , papply = require('jinang/papply')
+    , safeClone = require('jinang/safeClone')
     , split = require('jinang/split')
-    
+
     /* in-file */
-    , ifUndefined = function() {
+    
+    , ifUndefined = function () {
         let ret;
         for (let i = 0; i < arguments.length && util.isUndefined(ret); i++) {
             ret = arguments[i];
@@ -24,20 +27,28 @@ const MODULE_REQUIRE = 1
         name.trim().split(/[\s,]+/).forEach(part => {
             if (part.startsWith('--')) {
                 names.push(part.substr(2));
-            }
-            else if (part.startsWith('-')) {
+            } else if (part.startsWith('-')) {
                 absorb(names, part.substr(1).split(''));
-            }
-            else if (!prefixedWithDash) {
+            } else if (!prefixedWithDash) {
                 names.push(part);
             }
         });
         return names;
-    }
-    ;
+    };
 
-function parseColumn(desc) {
-    let column = { 
+function throwParseError(error, catcher) {
+    if (typeof error == 'string') {
+        error = new Error(error);
+    }
+    if (catcher) {
+        catcher(error);
+    } else {
+        throw error;
+    }
+}
+
+function parseColumn(desc, catcher) {
+    let column = {
         name: null,
         alias: [],
         assignable: undefined,
@@ -63,7 +74,7 @@ function parseColumn(desc) {
             column.alias = splitIntoNames(parts[0]);
             column.name = column.alias.shift();
         }
-        
+
         let decos = parts.slice(1).map(deco => deco.toLowerCase());
         let notdeco = false;
         decos.forEach((deco) => {
@@ -122,7 +133,7 @@ function parseColumn(desc) {
                 case 'comment':
                     column.comment = argsText;
                     break;
-            
+
                 default:
                     break;
             }
@@ -130,24 +141,22 @@ function parseColumn(desc) {
             // Reset the NOT decorator.
             notdeco = false;
         });
-    }
-    else {
+    } else {
         column = Object.assign(column, desc);
-        
+
         if (typeof column.alias == 'string') {
             column.alias = splitIntoNames(column.alias);
-        }
-        else if (!column.alias) {
+        } else if (!column.alias) {
             column.alias = [];
         }
-        
+
         let names = splitIntoNames(column.name);
         column.name = names.shift();
-        column.alias = names.concat(column.alias);       
+        column.alias = names.concat(column.alias);
     }
 
     column.multiple = ifUndefined(column.multiple, false);
-    if (column.multiple && 
+    if (column.multiple &&
         (column.nullable === true || column.assignable === false || column.overwrite === true)) {
         throw new Error(`option MULTIPLE should also be ASSIGNABLE, NOT NULLABLE and NOT overwrite: ${column.name}`);
     }
@@ -156,6 +165,150 @@ function parseColumn(desc) {
     column.nullable = ifUndefined(column.nullable, true);
 
     return column;
+}
+
+function parseOptions(raw, def) {
+    // 从原始选项数据中提取指定位置的值。
+    let consumeOption = (index, novalue) => {
+        let option = raw.options.splice(index, 1)[0];
+        let value = option.value;
+        if (typeof value == 'number') {
+            if (novalue) {
+                value = true;
+            } else {
+                let index = value;
+                value = raw.$[index];
+                raw.$[index] = null;
+            }
+        }
+        return value;
+    };
+
+    let parsedOptions = {};
+
+    for (let I = 0; I < def.options.length; I++) {
+        const column = def.options[I];
+
+        let caseSensitive = ifUndefined(column.caseSensitive, def.caseSensitive);
+        let overwrite = ifUndefined(column.overwrite, def.overwrite);
+
+        // *names* includes formal name and alias.
+        const names = [column.name].concat(column.alias);
+        const names_lc = caseSensitive ? null : names.map(name => name.toLowerCase());
+        const names_notation = names.map(name => (name.length > 1 ? '--' : '-') + name).join(', ');
+
+        let found = false;
+        let value = column.multiple ? [] : null;
+        for (let i = 0; i < raw.options.length; i++) {
+            let option = raw.options[i];
+            let matched = caseSensitive ? names.includes(option.name) : names_lc.includes(option.name.toLowerCase());
+            if (matched) {
+                if (column.multiple) {
+                    let v = consumeOption(i);
+                    if (typeof v == 'boolean') {
+                        throw new Error(`option need to be value: ${names_notation}`);
+                    }
+                    value.push(v);
+                    i--;
+                } else {
+                    if (found && !overwrite) {
+                        throw new Error(`option not allowed to be duplicated: ${names_notation}`);
+                    }
+                    value = consumeOption(i, !column.assignable);
+                    i--;
+                }
+                found = true;
+            }
+        }
+
+        if (found) {
+            parsedOptions[column.name] = value;
+        }
+
+        if (found && !column.nullable && typeof value == 'boolean') {
+            throw new Error(`option need to be valued: ${names_notation}`);
+        }
+
+        if (!found && column.required) {
+            throw new Error(`option required: ${names_notation}`);
+        }
+
+        if (!found && column.default) {
+            parsedOptions[column.name] = column.default;
+        }
+
+        if (found && column.parser) {
+            parsedOptions[column.name] = column.parser(parsedOptions[column.name]);
+        }
+    }
+
+    if (raw.options.length) {
+        if (def.explicit) {
+            let names_notation = raw.options.map(option => (option.name.length > 1 ? '--' : '-') + option.name);
+            throw new Error(`unknown options: ${names_notation}`);
+        } else {
+            while (raw.options.length) {
+                let option = raw.options[0];
+                parsedOptions[option.name] = consumeOption(0);
+            }
+        }
+    }
+
+    parsedOptions.$ = raw.$.filter(v => v != null);
+
+    return parsedOptions;
+}
+
+function parseRaw(args, def) {
+    let raw = {
+        options: [],
+        $: []
+    };
+    args.forEach(arg => {
+        if (/^(-{1,2})(no-)?([^=]+)(=(.+))?$/i.test(arg)) {
+            let dash = RegExp.$1;
+            let no = RegExp.$2;
+            let name = RegExp.$3;
+            let value = RegExp.$4 ? RegExp.$5 : true;
+
+            if (no) {
+                if (dash.length == 1 || value !== true) {
+                    throw new Error(`incomprehensible option: ${arg}`);
+                }
+                value = false;
+                raw.options.push({
+                    name,
+                    value
+                });
+            } else if (dash.length == 1) {
+                let names = name.split('');
+                let lastname = names.pop();
+                names.forEach(shortname => raw.options.push({
+                    name: shortname,
+                    value: true
+                }));
+                raw.options.push({
+                    name: lastname,
+                    value
+                });
+            } else {
+                raw.options.push({
+                    name,
+                    value
+                });
+            }
+        } else {
+            raw.$.push(arg);
+
+            // 如果上一个选项非 --no-* 形式选项，则标记其对应的值的序号。
+            let last = raw.options[raw.options.length - 1];
+            if (last && last.value === true) {
+                last.value = raw.$.length - 1;
+            }
+        }
+    });
+
+    return raw;
 }
 
 /**
@@ -170,158 +323,82 @@ function parseCommand(cmd, def) {
     let argv = null;
 
     if (typeof arguments[0] == 'string') {
-        argv = split(arguments[0], /\s+/, [ '"', "'" ], '\\');
+        argv = split(arguments[0], /\s+/, ['"', "'"], '\\');
         def = arguments[1];
-    }
-    else if (arguments[0] instanceof Array) {
+    } else if (arguments[0] instanceof Array) {
         argv = arguments[0].slice(0);
         def = arguments[1];
-    }
-    else {
+    } else {
         argv = process.argv.slice(1);
         def = arguments[0];
     }
-    
+
     if (def instanceof Array) {
-        def = { options: def }
+        def = {
+            options: def
+        }
     }
     def = Object.assign({
         overwrite: true,
         caseSensitive: true,
         explicit: false,
+        catcher: null,
+        groups: null,
         options: [],
     }, def);
 
-    def.options = def.options.map(parseColumn);
-    
-    let [ name, ...args ] = argv;
+    let [name, ...args] = argv;
 
     // ---------------------------
     // Main Process
 
-    let raw = { options: [], $: [] };
-    args.forEach(arg => {
-        if (/^(-{1,2})(no-)?([^=]+)(=(.+))?$/i.test(arg)) {
-            let dash  = RegExp.$1;
-            let no    = RegExp.$2;
-            let name  = RegExp.$3;
-            let value = RegExp.$4 ? RegExp.$5 : true;
-
-            if (no) {
-                if (dash.length == 1 || value !== true) {
-                    throw new Error(`incomprehensible option: ${arg}`);
-                }
-                value = false;
-                raw.options.push({ name, value });
-            }
-            else if (dash.length == 1) {
-                let names = name.split('');
-                let lastname = names.pop();
-                names.forEach(shortname => raw.options.push({ name: shortname, value: true }));
-                raw.options.push({ name: lastname, value });
-            }
-            else {
-                raw.options.push({ name, value });
-            }
+    let throws = (fn) => {
+        try {
+            return fn();
+        } catch (ex) {
+            if (def.catcher) def.catcher(ex);
+            else throw ex;    
         }
-        else {
-            raw.$.push(arg);
-
-            // 如果上一个选项非 --no-* 形式选项，则标记其对应的值的序号。
-            let last = raw.options[ raw.options.length - 1 ];
-            if (last && last.value === true) {
-                last.value = raw.$.length - 1;
-            }
-        }
-    });
-
-    // 从原始选项数据中提取指定位置的值。
-    let consumeOption = (index, novalue) => {
-        let option = raw.options.splice(index, 1)[0];
-        let value = option.value;
-        if (typeof value == 'number') {
-            if (novalue) {
-                value = true;
-            }
-            else {
-                let index = value;
-                value = raw.$[index];
-                raw.$[index] = null;
-            }
-        }
-        return value;
     };
 
-    let parsedOptions = {};
+    let raw = throws(() => parseRaw(args, def));
+    let parsedOptions = null;
+    if (def.groups && def.groups.length) {
+        let reasons = [];
+        let maxMatching = 0;
+        for (let i = 0; i < def.groups.length; i++) {
+            let rawcopy = safeClone(raw);
+            let parsed = null;
+            let matching = 0;
+            def.options = throws(() => def.groups[i].map(parseColumn));
+            try {
+                parsed = parseOptions(rawcopy, def);
 
-    for (let I = 0; I < def.options.length; I++) {
-        const column = def.options[I];
-        
-        let caseSensitive = ifUndefined(column.caseSensitive, def.caseSensitive);
-        let overwrite = ifUndefined(column.overwrite, def.overwrite);
-
-        // *names* includes formal name and alias.
-        const names = [ column.name ].concat(column.alias);
-        const names_lc = caseSensitive ? null : names.map(name => name.toLowerCase());
-        const names_notation = names.map(name => (name.length > 1 ? '--' : '-') + name).join(', ');           
-        
-        let found = false;
-        let value = column.multiple ? [] : null;
-        for (let i = 0; i < raw.options.length; i++) {
-            let option = raw.options[i];
-            let matched = caseSensitive ? names.includes(option.name) : names_lc.includes(option.name.toLowerCase());
-            if (matched) {
-                if (column.multiple) {
-                    let v = consumeOption(i);
-                    if (typeof v == 'boolean') {
-                        throw new Error(`option need to be value: ${names_notation}`);
-                    }
-                    value.push(v);
-                    i--;
+                // 取匹配度最高的选项组。
+                def.options.forEach(option => {
+                    if (parsed.hasOwnProperty(option.name)) matching++;
+                });
+                if (matching > maxMatching) {
+                    maxMatching = matching;
+                    parsedOptions = parsed;
                 }
-                else {
-                    if (found && !overwrite) {
-                        throw new Error(`option not allowed to be duplicated: ${names_notation}`);
-                    }
-                    value = consumeOption(i, !column.assignable);
-                    i--;
-                }
-                found = true;
+            } catch (ex) {
+                reasons.push(ex);
             }
         }
-        parsedOptions[column.name] = value;
 
-        if (found && !column.nullable && typeof value == 'boolean') {
-            throw new Error(`option need to be valued: ${names_notation}`);
-        }
-
-        if (!found && column.required) {
-            throw new Error(`option required: ${names_notation}`);
-        }
-
-        if (!found && column.default) {
-            definedOptions[propertyName] = column.default;
-        }
-
-        if (found && column.parser) {
-            definedOptions[propertyName] = column.parser(definedOptions[propertyName]);
+        if (!parsedOptions) {
+            let error = new Error('None of the option groups matched');
+            error.reasons = reasons;
+            if (def.catcher) def.catcher(error);
+            else throw error;
         }
     }
-
-    if (raw.options.length) {
-        if (def.explicit) {
-            let names_notation = raw.options.map(option => (option.name.length > 1 ? '--' : '-') + option.name);
-            throw new Error(`unknown options: ${names_notation}`);
-        }
-        else {
-            while (raw.options.length) {
-                let option = raw.options[0];
-                parsedOptions[ option.name ] = consumeOption(0);
-            }
-        }
+    else {
+        def.options = throws(() => def.options.map(parseColumn));
+        parsedOptions = throws(() => parseOptions(raw, def));
     }
-
-    parsedOptions.$ = raw.$.filter(v => v != null);
+    
     return parsedOptions;
 }
 
